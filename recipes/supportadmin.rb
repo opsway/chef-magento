e0af6c57752b5b5d9319015b6dc03b224bcde771
Andrey Zherebchenko <andy.zherebchenko@gmail.com>
2014-03-25 02:07:20 +0200

    magerun exit code ignored

--- recipes/supportadmin.rb
+++ recipes/supportadmin.rb
@@ -8,6 +8,7 @@ execute "Reset support admin password" do
 	command <<-EOF
 		/usr/local/bin/n98-magerun.phar --root-dir=#{node['magento']['docroot']} admin:user:create #{node['magento']['supportuser']['username']} #{node['magento']['supportuser']['email']} #{node['magento']['supportuser']['password']} #{random_password} Support Admin #{node['magento']['supportuser']['role']}
 		/usr/local/bin/n98-magerun.phar --root-dir=#{node['magento']['docroot']} admin:user:change-password #{node['magento']['supportuser']['username']} #{random_password}
+   echo
 	EOF
 	returns [0,1]
 	only_if do File.exist?("/usr/local/bin/n98-magerun.phar") end
