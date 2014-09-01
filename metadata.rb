337b24665e20bc243ceff6e6fd9ccc3c74f2d0dc
Andriy Samilyak <samilyak@gmail.com>
2014-09-01 15:00:48 +0300

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'samilyak@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures PHP for Magento'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.2'
+version          '0.6.0'
 
 depends "php"
 depends "n98-magerun"
